d0e34b9c6fdfc53add348ac030e718bd021069cc
Oleg Buyalski <oleg.buyalski@gmail.com>
2019-07-17 10:32:53 +0300

    Updates

---
+++ precache-manifest.7b07b0be50114dc65322bcd593b8f46c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e7c05c4e48bea2d6bf2d",
+    "url": "/todoist/static/css/main.cbada3e2.chunk.css"
+  },
+  {
+    "revision": "e7c05c4e48bea2d6bf2d",
+    "url": "/todoist/static/js/main.e7c05c4e.chunk.js"
+  },
+  {
+    "revision": "28f50e47e49d8941de08",
+    "url": "/todoist/static/js/runtime~main.28f50e47.js"
+  },
+  {
+    "revision": "6468a99a95e6e086abec",
+    "url": "/todoist/static/js/2.6468a99a.chunk.js"
+  },
+  {
+    "revision": "00b81c12c7e362cd8a6d82149ae6b664",
+    "url": "/todoist/index.html"
+  }
+];
